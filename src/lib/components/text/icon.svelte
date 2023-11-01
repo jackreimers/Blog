8387922df72d160eb87d4833056f868f2421fec9
Jack Reimers <jack.reimers.00@gmail.com>
2023-11-01 19:32:08 +1000

    Fix Prettier configuration

--- src/lib/components/text/icon.svelte
+++ src/lib/components/text/icon.svelte
@@ -1,12 +1,12 @@
-<script lang='ts'>
+<script lang="ts">
 	export let icon: string = 'question_mark';
 	export let weight: number = 600;
 	export let classes: string = '';
 </script>
 
-<div class='flex items-center'>
+<div class="flex items-center">
 	<span
-		class='material-symbols-rounded {classes}'
+		class="material-symbols-rounded {classes}"
 		style="font-variation-settings: 'wght' {weight}"
 	>
 		{icon}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<div class="bg-white px-8 text-4xl"></div>
+<p></p>
