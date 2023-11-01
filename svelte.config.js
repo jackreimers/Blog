61daea5ef56f4a690abc5974b5d9cb69a30cda47
Jack Reimers <jack.reimers.00@gmail.com>
2023-11-01 19:46:10 +1000

    Remove paths from Svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -9,10 +9,7 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html',
 			pages: 'build/'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		})
 	}
 };
 
