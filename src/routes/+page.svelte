eb00eed1f97c47fdba759c05f45b1e5cc72a70e2
Jack Reimers <jack.reimers.00@gmail.com>
2023-11-06 07:58:48 +1000

    Add https to local environment and set base URLs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import BlogHeader from '$lib/components/layout/header-blog.svelte';
+
+	import { PUBLIC_APP_ROOT } from '$env/static/public';
 </script>
 
 <BlogHeader />
+
+<p>{PUBLIC_APP_ROOT}</p>
--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,3 +1,5 @@
+import { PUBLIC_APP_ROOT } from '$env/static/public';
+
 interface PageLoadRequest {
 	slug: string;
 }
@@ -10,12 +12,7 @@ interface PageLoadResult {
 //TODO: Figure out what the type is for params
 //@ts-ignore
 export async function load({ params }): PageLoadResult {
-	//TODO: Figure out how to use relative paths and HTTPS here - https://github.com/sveltejs/kit/issues/3479 | https://kit.svelte.dev/docs/modules
-	//http://localhost:5173
-
-	// @ts-ignore
-	FetchEvent;
-	const blogContent = await fetch('/posts/test.md');
+	const blogContent = await fetch(`${PUBLIC_APP_ROOT}/posts/test.md`);
 	return { content: await blogContent.text() };
 
 	//throw error(404, 'Not found');
