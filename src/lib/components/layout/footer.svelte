9a5cb84e6163cb8eddfe8466ee3a06d51e8880ce
Jack Reimers <jack.reimers.00@gmail.com>
2023-11-20 11:21:44 +1000

    Add footer component

---
+++ src/lib/components/layout/footer.svelte
@@ -0,0 +1,34 @@
+<footer>
+	<!-- TODO: Decide whether to include this or not -->
+	<!--
+    <div class="container mx-auto flex w-full gap-8 px-8 py-8">
+        <div class="flex-1">
+            <p class="mb-3 text-2xl font-medium">Header</p>
+            <p>
+                Text goes here blah blah blah and on and more and just a whole lotta text type beat
+                up in this div element right now.
+            </p>
+        </div>
+        <div class="flex-1">
+            <p class="mb-3 text-2xl font-medium">Header</p>
+            <p>
+                Text goes here blah blah blah and on and more and just a whole lotta text type beat
+                up in this div element right now.
+            </p>
+        </div>
+        <div class="flex-1">
+            <p class="mb-3 text-2xl font-medium">Header</p>
+            <p>
+                Text goes here blah blah blah and on and more and just a whole lotta text type beat
+                up in this div element right now.
+            </p>
+        </div>
+    </div>
+    -->
+
+	<!-- Social buttons -->
+	<div class="h-[50px] w-full border-y border-gray-300 bg-gray-100"></div>
+	<div class="w-full text-center">
+		<p class="my-2 font-medium">Copyright © 2023</p>
+	</div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import '../app.css';
 	import Header from '$lib/components/layout/header.svelte';
+	import Footer from '$lib/components/layout/footer.svelte';
 
 	//TODO: https://kit.svelte.dev/docs/load#$page-data
 </script>
 
-<Header />
+<div class="flex h-screen flex-col">
+	<Header />
 
-<div class="container mx-auto px-8">
-	<slot />
+	<div class="container mx-auto flex-1 px-8">
+		<slot />
+	</div>
+
+	<Footer />
 </div>
