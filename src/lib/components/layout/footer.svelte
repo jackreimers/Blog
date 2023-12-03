1a9ae0af5ec9942159da08739daa80e907b52191
Jack Reimers <jack.reimers.00@gmail.com>
2023-12-03 13:49:31 +1000

    Adding to footer

--- src/lib/components/layout/footer.svelte
+++ src/lib/components/layout/footer.svelte
@@ -1,4 +1,12 @@
-<footer>
+<script>
+	import { PUBLIC_APP_ROOT } from '$env/static/public';
+	import Button from '$lib/components/buttons/button.svelte';
+	import Icon from '$lib/components/text/icon.svelte';
+
+	const url = `${PUBLIC_APP_ROOT}/icons/github.svg`;
+</script>
+
+<footer class="mt-32">
 	<!-- TODO: Decide whether to include this or not -->
 	<!--
     <div class="container mx-auto flex w-full gap-8 px-8 py-8">
@@ -26,9 +34,21 @@
     </div>
     -->
 
-	<!-- Social buttons -->
-	<div class="h-[50px] w-full border-y border-gray-300 bg-gray-100"></div>
+	<!-- Social buttons 
+    <div class="h-[50px] w-full border-y border-gray-300 bg-gray-100"></div>
+    -->
+	<div class="flex w-full justify-center gap-3">
+		<Button classes="rounded-full bg-gray-100 p-4">
+			<Icon icon="mail" classes="text-2xl" />
+		</Button>
+		<Button classes="rounded-full bg-gray-100 p-4">
+			<Icon icon="question_mark" classes="text-2xl" />
+		</Button>
+		<Button classes="rounded-full bg-gray-100 p-4">
+			<Icon icon="question_mark" classes="text-2xl" />
+		</Button>
+	</div>
 	<div class="w-full text-center">
-		<p class="my-2 font-medium">Copyright © 2023</p>
+		<p class="my-4 font-medium">Copyright © 2023</p>
 	</div>
 </footer>
