e8ee2f4e5aef97a0b68e6f17ea37d9e614798e8b
Jack Reimers <jack.reimers.00@gmail.com>
2023-12-03 23:05:29 +1000

    Add projects page placeholder

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Button from '$lib/components/buttons/button.svelte';
+	import Icon from '$lib/components/text/icon.svelte';
+</script>
+
+<div class="mb-16 max-w-prose">
+	<p class="mb-4 text-6xl font-bold">Oops...</p>
+	<p>This page is still under construction!</p>
+</div>
+<Button href="/" classes="bg-gray-100 py-2.5 pl-3 pr-4 font-semibold">
+	<Icon icon="arrow_left_alt" />
+	Back
+</Button>
