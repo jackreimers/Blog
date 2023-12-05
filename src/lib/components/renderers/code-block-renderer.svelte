607e565d6dee5efcd5d05d626ea8de4b232c6ad0
Jack Reimers <jack.reimers.00@gmail.com>
2023-12-05 18:00:19 +1000

    Add copy button to code block

--- src/lib/components/renderers/code-block-renderer.svelte
+++ src/lib/components/renderers/code-block-renderer.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
 	//TODO: Try to get the autoloader working
-	//TODO: Looks like Prism has a copy to clipboard plugin, try to get that working
+	import Button from '$lib/components/buttons/button.svelte';
+	import Icon from '$lib/components/text/icon.svelte';
 	import Prism from 'prismjs';
 	import 'prismjs/themes/prism.css';
 	import 'prismjs/components/prism-csharp';
 
 	export let lang: string = '';
 	export let text: string = '';
+
+	function copy() {
+		navigator.clipboard.writeText(text);
+	}
 </script>
 
-<code class="{lang} whitespace-pre-wrap">
-	{@html Prism.highlight(text, Prism.languages[lang], lang)}
-</code>
+<div class="relative inline-block rounded bg-gray-50 py-4 pl-6 pr-20">
+	<Button
+		onClick={copy}
+		classes="absolute right-4 top-4 p-2 text-gray-400 hover:bg-gray-200 hover:text-gray-700"
+	>
+		<Icon icon="content_copy" weight={400} />
+	</Button>
+	<code class="{lang} whitespace-pre-wrap">
+		{@html Prism.highlight(text, Prism.languages[lang], lang)}
+	</code>
+</div>
