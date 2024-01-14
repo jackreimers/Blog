f55e5abdc097c002eea5eac66e8d870c2431bc78
Jack Reimers <jack.reimers.00@gmail.com>
2024-01-14 17:46:17 +1000

    Add about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,17 +1,50 @@
 <script lang="ts">
-	import Button from '$lib/components/buttons/button.svelte';
+	import { PUBLIC_APP_ROOT } from '$env/static/public';
 	import Icon from '$lib/components/text/icon.svelte';
+	import Button from '$lib/components/buttons/button.svelte';
+
+	const url = `${PUBLIC_APP_ROOT}/images/headshot.png`;
 </script>
 
 <svelte:head>
 	<title>Jack Reimers | About</title>
 </svelte:head>
 
-<div class="mb-16 max-w-prose">
-	<p class="mb-4 text-6xl font-bold">Oops...</p>
-	<p>This page is still under construction!</p>
+<div class="flex flex-col">
+	<div>
+		<div
+			class="mx-auto h-[170px] w-[170px] rounded-full border border-gray-300 bg-cover"
+			style="background-image: url({url})"
+		/>
+		<div class="mt-4 text-center">
+			<h2 class="text-2xl font-semibold">Jack Reimers</h2>
+			<p>Software Engineer</p>
+			<p>SSW</p>
+		</div>
+		<div class="my-6 h-[2px] w-full rounded-full bg-gray-200" />
+		<div class="flex w-full justify-center gap-3">
+			<Button classes="rounded-full bg-gray-100 p-4 hover:bg-gray-200">
+				<Icon icon="mail" classes="text-2xl" />
+			</Button>
+			<Button classes="rounded-full bg-gray-100 p-4 hover:bg-gray-200">
+				<Icon icon="question_mark" classes="text-2xl" />
+			</Button>
+			<Button classes="rounded-full bg-gray-100 p-4 hover:bg-gray-200">
+				<Icon icon="question_mark" classes="text-2xl" />
+			</Button>
+		</div>
+		<div class="my-6 h-[2px] w-full rounded-full bg-gray-200" />
+	</div>
+	<div class="">
+		<p class="mb-5">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+			incididunt ut labore et dolore magna aliqua. Posuere urna nec tincidunt praesent semper
+			feugiat nibh.
+		</p>
+		<p class="mb-5">
+			In vitae turpis massa sed elementum tempus. Laoreet id donec ultrices tincidunt arcu non
+			sodales neque sodales. Velit laoreet id donec ultrices. Pulvinar neque laoreet
+			suspendisse interdum consectetur libero id. Mi bibendum neque egestas congue.
+		</p>
+	</div>
 </div>
-<Button href="/" classes="bg-gray-100 py-2.5 pl-3 pr-4 font-semibold">
-	<Icon icon="arrow_left_alt" />
-	Back
-</Button>
