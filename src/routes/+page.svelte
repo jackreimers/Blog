33d25afec5e65589053c451afd8764fa380e8f46
Jack Reimers <jack.reimers.00@gmail.com>
2024-01-19 20:44:43 +1000

    Improve homepage cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+	import { MoveRight } from 'lucide-svelte';
+
 	import Button from '$lib/components/buttons/button.svelte';
-	import Icon from '$lib/components/text/icon.svelte';
 </script>
 
 <svelte:head>
@@ -21,81 +22,37 @@
 	</p>
 </div>
 <div class="flex flex-col gap-4">
-	<Button href="blog">
-		<div class="w-full rounded-[4px] bg-gradient-to-r from-blue-600 to-blue-800 p-0.5">
-			<div class="h-full w-full rounded-[2px] bg-white">
-				<span class="flex flex-col px-3.5 py-2.5">
-					<span class="font-semibold">Blog</span>
-					<span class="text-sm text-gray-500">Read my latest posts.</span>
-				</span>
-			</div>
-		</div>
-	</Button>
-	<Button href="projects">
-		<div class="w-full rounded-[4px] bg-gradient-to-r from-blue-600 to-blue-800 p-0.5">
-			<div class="h-full w-full rounded-[2px] bg-white">
-				<span class="flex flex-col px-3.5 py-2.5">
-					<span class="font-semibold">Projects</span>
-					<span class="text-sm text-gray-500">View projects I've worked on.</span>
-				</span>
-			</div>
-		</div>
-	</Button>
-	<Button href="about">
-		<div class="w-full rounded-[4px] bg-gradient-to-r from-blue-600 to-blue-800 p-0.5">
-			<div class="h-full w-full rounded-[2px] bg-white">
-				<span class="flex flex-col px-3.5 py-2.5">
-					<span class="font-semibold">About</span>
-					<span class="text-sm text-gray-500">Learn more about me.</span>
-				</span>
-			</div>
-		</div>
-	</Button>
-</div>
-
-<!--
-<div class="grid w-full">
-	<Button
-		href="blog"
-		classes="flex w-full flex-col overflow-hidden rounded border border-gray-200"
-	>
-		<span class="flex justify-center bg-gray-100 py-8 align-middle">
-			<Icon icon="newsmode" weight={300} classes="text-7xl text-gray-400" />
-		</span>
-		<span class="h-[2px] w-full bg-gradient-to-r from-blue-600 to-blue-950" />
-		<span class="flex flex-col px-3.5 py-2.5">
+	<Button href="blog" classes="group flex bg-gray-100 px-3.5 py-2.5">
+		<span class="flex flex-1 flex-col">
 			<span class="font-semibold">Blog</span>
 			<span class="text-sm text-gray-500">Read my latest posts.</span>
 		</span>
-	</Button>
-	<Button
-		href="blog"
-		classes="flex w-full flex-col overflow-hidden rounded border border-gray-200"
-	>
-		<span class="flex justify-center bg-gray-100 py-8 align-middle">
-			<Icon icon="handyman" weight={300} classes="text-7xl text-gray-400" />
+		<span
+			class="mr-3 flex items-center text-gray-400 transition-all duration-300 group-hover:mr-1.5 group-hover:text-gray-600"
+		>
+			<MoveRight />
 		</span>
-		<span class="h-[2px] w-full bg-gradient-to-r from-blue-600 to-blue-950" />
-		<span class="flex flex-col px-3.5 py-2.5">
+	</Button>
+	<Button href="projects" classes="group flex bg-gray-100 px-3.5 py-2.5">
+		<span class="flex flex-1 flex-col">
 			<span class="font-semibold">Projects</span>
-			<span class="text-sm text-gray-500">View projects I've worked on.</span>
+			<span class="text-sm text-gray-500">See my past projects.</span>
+		</span>
+		<span
+			class="mr-3 flex items-center text-gray-400 transition-all duration-300 group-hover:mr-1.5 group-hover:text-gray-600"
+		>
+			<MoveRight />
 		</span>
 	</Button>
-	<Button
-		href="blog"
-		classes="flex w-full flex-col overflow-hidden rounded border border-gray-200"
-	>
-		<span class="flex justify-center bg-gray-100 py-8 align-middle">
-			<Icon icon="person" weight={300} classes="text-7xl text-gray-400" />
-		</span>
-		<span class="h-[2px] w-full bg-gradient-to-r from-blue-600 to-blue-950" />
-		<span class="flex flex-col px-3.5 py-2.5">
+	<Button href="about" classes="group flex bg-gray-100 px-3.5 py-2.5">
+		<span class="flex flex-1 flex-col">
 			<span class="font-semibold">About</span>
 			<span class="text-sm text-gray-500">Learn more about me.</span>
 		</span>
+		<span
+			class="mr-3 flex items-center text-gray-400 transition-all duration-300 group-hover:mr-1.5 group-hover:text-gray-600"
+		>
+			<MoveRight />
+		</span>
 	</Button>
 </div>
-
-<Button href="blog">Go to blog</Button>
-<Button href="blog/test">Go to blog post</Button>
--->
