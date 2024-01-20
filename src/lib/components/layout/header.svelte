a0d100135b9848d24ad7717b9e302b2ee225057b
Jack Reimers <jack.reimers.00@gmail.com>
2024-01-20 21:12:38 +1000

    Add links to slide out navigation

--- src/lib/components/layout/header.svelte
+++ src/lib/components/layout/header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 
 	import Blocker from '$lib/components/layout/blocker.svelte';
 	import Button from '$lib/components/buttons/button.svelte';
@@ -14,6 +15,11 @@
 
 	function onScroll() {
 		scrolled = window.scrollY > 50;
+	}
+
+	function routeTo(path: string) {
+		open = false;
+		goto(path);
 	}
 </script>
 
@@ -60,20 +66,49 @@
 	class="absolute right-0 top-0 z-20 h-full overflow-hidden bg-white transition-all duration-500"
 >
 	<div class="w-[320px]">
-		<div class="flex p-4">
-			<div class="flex-1" />
-			<Button
-				onClick={() => {
-					open = false;
-				}}
-				classes="rounded p-2 duration-500 hover:bg-gray-100"
-			>
-				<Icon
-					icon="close"
-					weight={600}
-					classes="bg-gradient-to-b from-red-600 to-red-800 bg-clip-text text-3xl text-transparent sm:text-4xl"
-				/>
-			</Button>
+		<div
+			class:ml-0={open}
+			class:ml-4={!open}
+			class=" p-4 transition-spacing delay-100 duration-700"
+		>
+			<div class="mb-8 flex">
+				<div class="flex-1" />
+				<Button
+					onClick={() => {
+						open = false;
+					}}
+					classes="rounded p-2 duration-500 hover:bg-gray-100"
+				>
+					<Icon
+						icon="close"
+						weight={600}
+						classes="bg-gradient-to-b from-red-600 to-red-800 bg-clip-text text-3xl text-transparent sm:text-4xl"
+					/>
+				</Button>
+			</div>
+			<div class="flex flex-col gap-4 px-4">
+				<Button
+					onClick={() => {
+						routeTo('/');
+					}}
+					classes="text-left text-2xl font-semibold"
+					>Home
+				</Button>
+				<Button
+					onClick={() => {
+						routeTo('projects');
+					}}
+					classes="text-left text-2xl font-semibold"
+					>Projects
+				</Button>
+				<Button
+					onClick={() => {
+						routeTo('about');
+					}}
+					classes="text-left text-2xl font-semibold"
+					>About
+				</Button>
+			</div>
 		</div>
 	</div>
 </div>
