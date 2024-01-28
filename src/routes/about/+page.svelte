8bdb563f7c7d682063274daed09bc23f62f26e6b
Jack Reimers <jack.reimers.00@gmail.com>
2024-01-28 22:16:37 +1000

    Optimise headshot image

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import { PUBLIC_APP_ROOT } from '$env/static/public';
 	import { Mail } from 'lucide-svelte';
 	import { Linkedin } from 'lucide-svelte';
 	import { Github } from 'lucide-svelte';
 
 	import Button from '$lib/components/buttons/button.svelte';
-
-	const url = `${PUBLIC_APP_ROOT}/images/headshot.png`;
 </script>
 
 <svelte:head>
@@ -14,10 +11,9 @@
 </svelte:head>
 
 <div class="md:flex md:gap-20">
-	<div class="mb-8 sm:mb-12 flex flex-col gap-4 md:mb-0">
+	<div class="mb-8 flex flex-col gap-4 sm:mb-12 md:mb-0">
 		<div
-			class="mx-auto h-[170px] w-[170px] rounded-full border border-gray-300 bg-cover md:mx-0"
-			style="background-image: url({url})"
+			class="mx-auto h-[170px] w-[170px] rounded-full border border-gray-300 bg-[url('/images/headshot.png')] bg-cover md:mx-0"
 		/>
 		<div class="text-center md:text-left">
 			<h2 class="text-3xl font-semibold">Jack Reimers</h2>
@@ -44,7 +40,7 @@
 			</Button>
 		</div>
 	</div>
-	<div class="rounded bg-gray-100 px-4 py-3.5 sm:px-5 sm:py-4 md:px-6 md:py-5 md:flex-1">
+	<div class="rounded bg-gray-100 px-4 py-3.5 sm:px-5 sm:py-4 md:flex-1 md:px-6 md:py-5">
 		<p class="mb-5">
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
 			incididunt ut labore et dolore magna aliqua. Posuere urna nec tincidunt praesent semper
