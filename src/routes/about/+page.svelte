9b6b63dd58ac500b86aa56da2820a7cdb0939ed2
Jack Reimers <jack.reimers.00@gmail.com>
2024-02-29 16:14:42 +1000

    Change about page layout

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -16,6 +16,64 @@
 	<h1 class="text-3xl font-bold sm:text-5xl lg:text-6xl">About</h1>
 </PageHeader>
 
+<div class="grid grid-cols-6 grid-rows-6 gap-5">
+	<div class="col-start-1 col-end-3 row-start-1 row-end-3 rounded bg-gray-100">
+		<img
+			src="/images/headshot-transparent.png"
+			alt="Professional headshot of Jack Reimers"
+			width="300"
+			class="mx-auto"
+		/>
+	</div>
+	<div class="col-start-3 col-end-5 rounded bg-gray-100">
+		<div class="flex h-full flex-col items-center justify-center gap-1.5">
+			<Icon icon="badge" classes="text-5xl" />
+			<p class="font-semibold">Software Engineer</p>
+		</div>
+	</div>
+	<div class="rounded bg-gray-100">
+		<div class="flex h-full flex-col items-center justify-center gap-1.5">
+			<Icon icon="work" classes="text-5xl" />
+			<p class="font-semibold">SSW</p>
+		</div>
+	</div>
+	<div></div>
+	<div class="rounded bg-gray-100">
+		<div class="flex h-full flex-col items-center justify-center gap-1.5">
+			<Icon icon="location_on" classes="text-5xl" />
+			<p class="font-semibold">Brisbane</p>
+		</div>
+	</div>
+</div>
+
+<!--
+<div class="flex gap-3 sm:gap-8">
+	<div class="flex flex-col rounded bg-gray-100 px-6 py-5 text-center">
+		<div class="mx-auto mb-4 overflow-hidden rounded-full border-2 bg-gray-50">
+			<img
+				src="/images/headshot-transparent.png"
+				alt="Professional headshot of Jack Reimers"
+				width="300"
+			/>
+		</div>
+		<p class="text-3xl font-semibold">Jack Reimers</p>
+		<p class="text-gray-500">Software Engineer</p>
+	</div>
+	<div class="flex-1">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+			incididunt ut labore et dolore magna aliqua. Posuere urna nec tincidunt praesent semper
+			feugiat nibh.
+		</p>
+		<div class="flex">
+			<div class="flex">
+				<Icon icon="location_on" />
+				<p>Brisbane, Australia</p>
+			</div>
+		</div>
+	</div>
+</div>
+
 <div class="mb-8 flex flex-col gap-4">
 	<div class="flex gap-3 sm:gap-4">
 		<div class="flex flex-1 overflow-hidden rounded bg-gray-100">
@@ -48,6 +106,7 @@
 		</div>
 	</div>
 	<div>
+		<p class="text-2xl font-bold sm:text-3xl">Jack Reimers</p>
 		<div
 			class="flex items-center gap-2 text-sm font-semibold leading-none sm:gap-2.5 sm:text-base"
 		>
@@ -65,3 +124,4 @@
 		labore et dolore magna aliqua. Posuere urna nec tincidunt praesent semper feugiat nibh.
 	</p>
 </div>
+-->
