e144a2c05f32e2818dbc23b5ba00bfee17b50476
Jack Reimers <jack.reimers.00@gmail.com>
2024-03-24 21:58:52 +1000

    Add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import PageHeader from '$lib/components/layout/page-header.svelte';
+	import Icon from '$lib/components/text/icon.svelte';
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>Jack Reimers | Oops...</title>
+</svelte:head>
+
+<PageHeader title="Oops..." />
