5f419d3f064a2206b759ce785c2dcc724e7ba2ae
Jack Reimers <jack.reimers.00@gmail.com>
2024-03-26 20:48:21 +1000

    Switch to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			fallback: '404.html',
-			pages: 'build/',
-			paths: {
-				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-			}
-		})
+		adapter: adapter({})
 	}
 };
 
