99cb1d8d43cc8886487a628bf7a3b7ff117a90db
Jack Reimers <jack.reimers.00@gmail.com>
2024-03-28 13:59:36 +1000

    Move dynamic route to page load

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -3,7 +3,7 @@ import { getBlogPost } from '$lib/common/functions';
 //TODO: Investigate if this can be prerendered
 export const prerender = false;
 
-/** @type {import('./$types').PageServerLoad} */
+/** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
 	return {
 		post: await getBlogPost(fetch, params.slug)
