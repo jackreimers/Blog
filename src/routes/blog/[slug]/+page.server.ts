8d125aea74e3e815f64415aed79dd0491dec015e
Jack Reimers <jack.reimers.00@gmail.com>
2024-03-28 14:01:58 +1000

    Revert "Move dynamic route to page load"
    This reverts commit 99cb1d8d43cc8886487a628bf7a3b7ff117a90db.

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -3,7 +3,7 @@ import { getBlogPost } from '$lib/common/functions';
 //TODO: Investigate if this can be prerendered
 export const prerender = false;
 
-/** @type {import('./$types').PageLoad} */
+/** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch, params }) {
 	return {
 		post: await getBlogPost(fetch, params.slug)
