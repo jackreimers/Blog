573c2c9bb8d8c1069e487cab55be983a92d2da54
Jack Reimers <jack.reimers.00@gmail.com>
2024-04-02 01:13:18 +1000

    Increase dropdown spacing

--- src/lib/components/clickable/dropdown-item.svelte
+++ src/lib/components/clickable/dropdown-item.svelte
@@ -7,7 +7,7 @@
 	on:click={() => {
 		onClick();
 	}}
-	class="rounded py-1 pl-3 pr-9 text-left transition-colors duration-200 sm:py-2 sm:pl-4 sm:pr-12 {active
+	class="rounded py-1.5 pl-3 pr-9 text-left transition-colors duration-200 sm:py-2.5 sm:pl-4 sm:pr-12 {active
 		? 'bg-gray-600 hover:bg-gray-500'
 		: 'hover:bg-gray-600'}"
 >
