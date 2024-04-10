c92f19bd8d15bc4232cf20ebe5d3cda1c88e6b22
Jack Reimers <jack.reimers.00@gmail.com>
2024-04-10 21:18:42 +1000

    Fix animation on blocker

--- src/lib/components/interactivity/blocker.svelte
+++ src/lib/components/interactivity/blocker.svelte
@@ -1,3 +1,11 @@
+<style>
+	button {
+		transition:
+			visibility 0.2s,
+			opacity 0.2s linear;
+	}
+</style>
+
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 
@@ -21,7 +29,7 @@
 
 <button
 	on:click={handleClick}
-	class="fixed left-0 top-0 h-full w-full bg-black bg-opacity-50 transition-opacity duration-200 {classes} {open
+	class="fixed left-0 top-0 h-full w-full bg-black bg-opacity-50 {classes} {open
 		? ''
 		: 'invisible opacity-0'}"
 />
