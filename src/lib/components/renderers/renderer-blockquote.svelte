40d9480543fe11afba7cb208961b9f2c94b8757d
Jack Reimers <jack.reimers.00@gmail.com>
2024-08-04 20:05:10 +1000

    Add blockquote renderer

---
+++ src/lib/components/renderers/renderer-blockquote.svelte
@@ -0,0 +1,6 @@
+<script></script>
+
+<blockquote class="mb-4 inline-block bg-orange-100 text-orange-900 max-w-prose p-4 rounded-lg">
+	<p class="text-lg font-semibold">Note</p>
+	<slot />
+</blockquote>
