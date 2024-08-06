8f5bc31307bb6d7ed0f2393151f77d7632fc8b24
Jack Reimers <jack.reimers.00@gmail.com>
2024-08-06 16:57:23 +1000

    Improve blockquote renderer

--- src/lib/components/renderers/renderer-blockquote.svelte
+++ src/lib/components/renderers/renderer-blockquote.svelte
@@ -1,6 +1,15 @@
-<script></script>
+<script>
+	import { AlertOctagon } from 'lucide-svelte';
+</script>
 
-<blockquote class="mb-4 inline-block bg-orange-100 text-orange-900 max-w-prose p-4 rounded-lg">
-	<p class="text-lg font-semibold">Note</p>
-	<slot />
+<blockquote
+	class="mb-5 inline-block max-w-prose border-l-4 border-blue-900 px-4 py-2 sm:mb-6 sm:py-3"
+>
+	<div class="inline-flex items-center gap-2 text-blue-900">
+		<AlertOctagon />
+		<p class="text-lg font-semibold">Important</p>
+	</div>
+	<div class="max-w-prose">
+		<slot />
+	</div>
 </blockquote>
