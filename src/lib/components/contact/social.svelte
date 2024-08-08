f4661469dff034c7d1474cb408938bcbad2a06aa
Jack Reimers <jack.reimers.00@gmail.com>
2024-08-09 00:28:51 +1000

    Use button component on social links

--- src/lib/components/contact/social.svelte
+++ src/lib/components/contact/social.svelte
@@ -1,27 +1,17 @@
 <script lang="ts">
 	import { Github, Linkedin, Mail } from 'lucide-svelte';
 	import HorizontalStack from '$lib/components/stacks/stack-horizontal.svelte';
+	import Button from '$lib/components/buttons/button.svelte';
 </script>
 
 <HorizontalStack>
-	<a
-		class="rounded bg-blue-900 p-2 text-white transition-colors duration-200 sm:p-3 mhover:hover:bg-blue-950"
-		href="mailto:me@jackreimers.dev"
-	>
+	<Button color="blueSolid" href="mailto:me@jackreimers.dev">
 		<Mail />
-	</a>
-	<a
-		class="rounded bg-blue-900 p-2 text-white transition-colors duration-200 sm:p-3 mhover:hover:bg-blue-950"
-		href="https://www.linkedin.com/in/jackreimers/"
-		target="_blank"
-	>
+	</Button>
+	<Button color="blueSolid" href="https://www.linkedin.com/in/jackreimers/" target="_blank">
 		<Linkedin />
-	</a>
-	<a
-		class="rounded bg-blue-900 p-2 text-white transition-colors duration-200 sm:p-3 mhover:hover:bg-blue-950"
-		href="https://github.com/jackreimers"
-		target="_blank"
-	>
+	</Button>
+	<Button color="blueSolid" href="https://github.com/jackreimers" target="_blank">
 		<Github />
-	</a>
+	</Button>
 </HorizontalStack>
