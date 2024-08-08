8e8f7d5e6e4b86345a61fcd0a8fca514ac51fb8f
Jack Reimers <jack.reimers.00@gmail.com>
2024-08-09 00:28:27 +1000

    Add inline option to stacks

--- src/lib/components/stacks/stack-horizontal.svelte
+++ src/lib/components/stacks/stack-horizontal.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	export let inline: boolean = false;
 	export let classes: string = '';
 </script>
 
-<div class="flex gap-2.5 sm:gap-3.5 {classes}">
-	<slot />
-</div>
+{#if inline}
+	<span class="flex gap-2.5 sm:gap-3.5 {classes}">
+		<slot />
+	</span>
+{:else}
+	<div class="flex gap-2.5 sm:gap-3.5 {classes}">
+		<slot />
+	</div>
+{/if}
