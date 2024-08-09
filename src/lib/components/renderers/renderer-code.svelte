9ddf259be3c090cd455f5bda1f286e40ecbf63af
Jack Reimers <jack.reimers.00@gmail.com>
2024-08-09 23:32:02 +1000

    Improve code block renderer

--- src/lib/components/renderers/renderer-code.svelte
+++ src/lib/components/renderers/renderer-code.svelte
@@ -6,6 +6,8 @@
 	export let lang: string = '';
 	export let text: string = '';
 
+	let lineNumberColor: string = 'rgb(156 163 175)';
+	let lineNumberBorderColor: string = 'rgb(209 213 219)';
 	let copied: boolean = false;
 
 	function copy() {
@@ -18,34 +20,39 @@
 	}
 </script>
 
-<div class="group mb-4">
+<div class="group mt-4">
 	<div
-		class="relative inline-block w-full overflow-hidden overflow-x-auto rounded text-gray-400 shadow"
+		class="relative inline-block w-full overflow-hidden overflow-x-auto rounded-lg text-sm text-gray-300 sm:text-base"
 	>
 		{#if lang === 'svelte'}
 			<HighlightSvelte code={text} let:highlighted>
 				<LineNumbers
-					--line-number-color="rgb(156 163 175)"
-					--border-color="rgb(156 163 175)"
-					hideBorder
+					--line-number-color={lineNumberColor}
+					--border-color={lineNumberBorderColor}
 					{highlighted}
 				/>
 			</HighlightSvelte>
 		{:else if lang === 'typescript'}
 			<Highlight language={typescript} code={text} let:highlighted>
 				<LineNumbers
-					--line-number-color="rgb(156 163 175)"
-					--border-color="rgb(156 163 175)"
+					--line-number-color={lineNumberColor}
+					--border-color={lineNumberBorderColor}
 					{highlighted}
 				/>
 			</Highlight>
 		{:else if lang === 'bash'}
-			<Highlight language={coffeescript} code={text} />
+			<Highlight language={coffeescript} code={text} let:highlighted>
+				<LineNumbers
+					--line-number-color={lineNumberColor}
+					--border-color={lineNumberBorderColor}
+					{highlighted}
+				/>
+			</Highlight>
 		{:else}
 			<p>Missing language!</p>
 		{/if}
 		<button
-			class="absolute right-3 top-3 rounded p-1.5 text-gray-400 opacity-0 transition-all duration-200 sm:right-4 sm:top-4 sm:p-2 mhover:hover:bg-gray-100 mhover:group-hover:opacity-100"
+			class="absolute right-3 top-3 z-10 rounded p-1.5 text-gray-400 opacity-0 transition-all duration-200 sm:right-4 sm:top-4 sm:p-2 mhover:hover:bg-orange-100 mhover:hover:text-orange-900 mhover:group-hover:opacity-100"
 			on:click={copy}
 		>
 			{#if copied}
