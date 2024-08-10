b30df204affcb91980baf7bd22ba153f3c55adca
Jack Reimers <jack.reimers.00@gmail.com>
2024-08-10 19:04:30 +1000

    Image optimisations

--- src/lib/components/contact/author.svelte
+++ src/lib/components/contact/author.svelte
@@ -6,11 +6,11 @@
 <div>
 	<a href="/about">
 		<HorizontalStack classes="items-center" inline={true}>
-			<Preload classes="overflow-hidden rounded-full" src="/images/about/headshot-128px.png">
+			<Preload classes="overflow-hidden rounded-full" src="/images/about/headshot.png">
 				<img
 					alt="Professional headshot of Jack Reimers"
 					class="h-14 w-14 bg-gray-200 sm:h-16 sm:w-16"
-					src="/images/about/headshot-128px.png"
+					src="/images/about/headshot.png"
 				/>
 			</Preload>
 			<div>
