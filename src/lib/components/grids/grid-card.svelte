6ddd9738c1b0bba5da0ebc223e956ece70fe06c9
Jack Reimers <jack.reimers.00@gmail.com>
2024-09-07 01:32:22 +1000

    Fix bug in card grid empty card calculation

--- src/lib/components/grids/grid-card.svelte
+++ src/lib/components/grids/grid-card.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div
-	class="grid grid-cols-1 gap-2.5 sm:grid-cols-2 sm:gap-3.5
+	class="grid w-full grid-cols-1 gap-2.5 sm:grid-cols-2 sm:gap-3.5
 	{horizontal ? 'md:grid-cols-1' : 'md:grid-cols-3'}
 	{classes}"
 >
@@ -17,7 +17,7 @@
 			<EmptyCard classes="hidden sm:inline-flex md:hidden" />
 		{/each}
 	{:else}
-		{#each { length: 3 - (itemCount % 3) } as _}
+		{#each { length: (3 - itemCount) % 3 } as _}
 			<EmptyCard classes="hidden md:inline-flex" />
 		{/each}
 	{/if}
