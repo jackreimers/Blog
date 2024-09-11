46a7d1479d8f9aab7c0df11336c174695c5bc9b3
Jack Reimers <jack.reimers.00@gmail.com>
2024-09-11 20:22:16 +1000

    Remove unnecessary vertical stacks

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -4,7 +4,6 @@
 	import Hero from '$lib/components/hero/hero.svelte';
 	import Container from '$lib/components/page/container.svelte';
 	import Section from '$lib/components/page/section.svelte';
-	import VerticalStack from '$lib/components/stacks/stack-vertical.svelte';
 	import TechnologyCard from '$lib/components/cards/card-technology.svelte';
 	import SocialLinks from '$lib/components/contact/links.svelte';
 	import SvelteMarkdown from 'svelte-markdown';
@@ -22,76 +21,72 @@
 </Hero>
 <Container>
 	<Section>
-		<VerticalStack>
-			<div class="markdown">
-				<SvelteMarkdown source={data.services.website} />
-			</div>
-			<div class="mt-6 flex flex-wrap gap-2.5 sm:mt-8 sm:gap-3.5">
-				<TechnologyCard
-					href="tags/html"
-					imageAlt="The HTML 5 logo."
-					imageHref={logoUrls.html}
-					title="HTML"
-				/>
-				<TechnologyCard
-					href="tags/css"
-					imageAlt="The CSS 3 logo."
-					imageHref={logoUrls.css}
-					title="CSS"
-				/>
-				<TechnologyCard
-					href="tags/javascript"
-					imageAlt="The JavaScript logo."
-					imageHref={logoUrls.javascript}
-					title="JavaScript"
-				/>
-				<TechnologyCard
-					href="tags/typescript"
-					imageAlt="The TypeScript logo."
-					imageHref={logoUrls.typescript}
-					title="TypeScript"
-				/>
-			</div>
-		</VerticalStack>
+		<div class="markdown">
+			<SvelteMarkdown source={data.services.website} />
+		</div>
+		<div class="mt-6 flex flex-wrap gap-2.5 sm:mt-8 sm:gap-3.5">
+			<TechnologyCard
+				href="tags/html"
+				imageAlt="The HTML 5 logo."
+				imageHref={logoUrls.html}
+				title="HTML"
+			/>
+			<TechnologyCard
+				href="tags/css"
+				imageAlt="The CSS 3 logo."
+				imageHref={logoUrls.css}
+				title="CSS"
+			/>
+			<TechnologyCard
+				href="tags/javascript"
+				imageAlt="The JavaScript logo."
+				imageHref={logoUrls.javascript}
+				title="JavaScript"
+			/>
+			<TechnologyCard
+				href="tags/typescript"
+				imageAlt="The TypeScript logo."
+				imageHref={logoUrls.typescript}
+				title="TypeScript"
+			/>
+		</div>
 	</Section>
 	<Section>
-		<VerticalStack>
-			<div class="markdown">
-				<SvelteMarkdown source={data.services.webApplication} />
-			</div>
-			<div class="mt-6 flex flex-wrap gap-2.5 sm:mt-8 sm:gap-3.5">
-				<TechnologyCard
-					href="tags/angular"
-					imageAlt="The Angular logo."
-					imageHref={logoUrls.angular}
-					title="Angular"
-				/>
-				<TechnologyCard
-					href="tags/blazor"
-					imageAlt="The Blazor logo."
-					imageHref={logoUrls.blazor}
-					title="Blazor"
-				/>
-				<TechnologyCard
-					href="tags/svelte"
-					imageAlt="The Svelte logo."
-					imageHref={logoUrls.svelte}
-					title="Svelte"
-				/>
-				<TechnologyCard
-					href="tags/azure"
-					imageAlt="The Azure logo."
-					imageHref={logoUrls.azure}
-					title="Azure"
-				/>
-				<TechnologyCard
-					href="tags/mssql"
-					imageAlt="The Microsoft SQL Server logo."
-					imageHref={logoUrls.mssql}
-					title="MSSQL"
-				/>
-			</div>
-		</VerticalStack>
+		<div class="markdown">
+			<SvelteMarkdown source={data.services.webApplication} />
+		</div>
+		<div class="mt-6 flex flex-wrap gap-2.5 sm:mt-8 sm:gap-3.5">
+			<TechnologyCard
+				href="tags/angular"
+				imageAlt="The Angular logo."
+				imageHref={logoUrls.angular}
+				title="Angular"
+			/>
+			<TechnologyCard
+				href="tags/blazor"
+				imageAlt="The Blazor logo."
+				imageHref={logoUrls.blazor}
+				title="Blazor"
+			/>
+			<TechnologyCard
+				href="tags/svelte"
+				imageAlt="The Svelte logo."
+				imageHref={logoUrls.svelte}
+				title="Svelte"
+			/>
+			<TechnologyCard
+				href="tags/azure"
+				imageAlt="The Azure logo."
+				imageHref={logoUrls.azure}
+				title="Azure"
+			/>
+			<TechnologyCard
+				href="tags/mssql"
+				imageAlt="The Microsoft SQL Server logo."
+				imageHref={logoUrls.mssql}
+				title="MSSQL"
+			/>
+		</div>
 	</Section>
 	<Section>
 		<div class="markdown">
