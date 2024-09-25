b828a1061f1a675acd37f1669ce5852092ef794c
Jack Reimers <jack.reimers.00@gmail.com>
2024-09-25 17:20:50 +1000

    Minor styling change

--- src/lib/components/renderers/renderer-blockquote.svelte
+++ src/lib/components/renderers/renderer-blockquote.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <blockquote class="my-4 flex max-w-prose">
-	<div class="w-[4px] rounded-lg bg-blue-900" />
+	<div aria-hidden="true" class="w-[4px] rounded-lg bg-blue-900" />
 	<div class="flex-1 py-3 pl-4">
 		<div class="inline-flex items-center gap-2">
 			<span class="text-blue-900">
 				<AlertOctagon />
 			</span>
-			<p class="!m-0 text-lg font-semibold">Important</p>
+			<p class="!m-0 text-lg font-semibold text-blue-900">Important</p>
 		</div>
 		<div class="max-w-prose font-medium text-gray-700 [&>p]:m-0">
 			<slot />
