3620b34374b54bd490db91c5ef5c72b42556f40f
Jack Reimers <jack.reimers.00@gmail.com>
2024-10-02 23:18:36 +1000

    Allow CORS on api endpoint

--- src/routes/api/built-tag/+server.ts
+++ src/routes/api/built-tag/+server.ts
@@ -3,5 +3,8 @@ export async function GET({ fetch }) {
 	const htmlResponse = await fetch('/content/html/built-tag.html');
 	const html = await htmlResponse.text();
 
-	return new Response(String(html));
+	const response = new Response(String(html));
+	response.headers.append('Access-Control-Allow-Origin', '*');
+
+	return response;
 }
