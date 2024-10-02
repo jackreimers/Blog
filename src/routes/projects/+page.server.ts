5e935e7e21e25f3e501695d6ecf41c67975e309a
Jack Reimers <jack.reimers.00@gmail.com>
2024-10-02 19:05:06 +1000

    Disable prerendering on projects routes

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -1,5 +1,8 @@
 import { getPosts } from '$lib/functions/functions.posts';
 
+//TODO: Reenable once the buttons are reenabled
+export const prerender = false;
+
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch }) {
 	const postsResult = await getPosts(fetch, 'projects');
--- src/routes/projects/[slug]/+page.server.ts
+++ src/routes/projects/[slug]/+page.server.ts
@@ -1,5 +1,8 @@
 import { getPost } from '$lib/functions/functions.posts';
 
+//TODO: Reenable once the buttons are reenabled
+export const prerender = false;
+
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch, params }) {
 	return {
