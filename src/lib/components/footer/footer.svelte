54abe4e32aad798fd16ae15eed0bffae4f2d81d6
Jack Reimers <jack.reimers.00@gmail.com>
2024-10-18 19:40:04 +1100

    Make footer responsive

--- src/lib/components/footer/footer.svelte
+++ src/lib/components/footer/footer.svelte
@@ -6,13 +6,13 @@
 
 <footer>
 	<Container>
-		<div class="flex flex-col justify-between border-t border-gray-300 py-4 sm:py-6">
+		<div class="flex flex-col-reverse sm:flex-row gap-4 text-center justify-between border-t border-gray-300 py-6">
 			<div class="text-gray-700">
 				<p class="tracking-tight">
 					Copyright © 2021 - {new Date().getFullYear()}. All rights reserved.
 				</p>
 			</div>
-			<div class="flex gap-3 text-gray-500">
+			<div class="flex gap-3 mx-auto sm:mx-0 text-gray-500">
 				<a
 					class="transition-colors duration-300 focus:text-gray-700 mhover:hover:text-gray-700"
 					href="https://www.linkedin.com/in/jackreimers"
