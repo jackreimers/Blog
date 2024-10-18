97f64b219addaacf203a7fe5cefbc404d4490c9f
Jack Reimers <jack.reimers.00@gmail.com>
2024-10-18 19:56:08 +1100

    Make subheader responsiveness

--- src/lib/components/subheader/subheader.svelte
+++ src/lib/components/subheader/subheader.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div class="mb-10 text-center {$$restProps.class}">
-	<h2 class="text-3xl font-bold tracking-tight">{title}</h2>
-	<p class="mt-4 text-lg tracking-tight text-gray-700">
+	<h2 class="text-xl font-bold tracking-tight sm:text-2xl md:text-3xl">{title}</h2>
+	<p class="mt-4 tracking-tight text-gray-700 sm:text-lg">
 		{subtitle}
 	</p>
 </div>
