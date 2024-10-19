efdb0029742a5ccb2a604493eb6fed3d31612787
Jack Reimers <jack.reimers.00@gmail.com>
2024-10-19 22:17:02 +1100

    Preload project mockup image

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { ArrowUpRightFromSquare } from 'lucide-svelte';
 	import SvelteMarkdown from 'svelte-markdown';
 	import Head from '$lib/components/seo/head.svelte';
 	import Container from '$lib/components/container/container.svelte';
 	import Section from '$lib/components/section/section.svelte';
 	import Hero from '$lib/components/hero/hero.svelte';
+	import Preload from '$lib/components/preload/preload.svelte';
 	import BasicButton from '$lib/components/buttons/basic/basic-button.svelte';
 	import AnchorRenderer from '$lib/components/renderers/renderer-anchor.svelte';
 	import ImageRenderer from '$lib/components/renderers/renderer-image.svelte';
 	import CodeRenderer from '$lib/components/renderers/renderer-code.svelte';
 	import BlockquoteRenderer from '$lib/components/renderers/renderer-blockquote.svelte';
-	import { onMount } from 'svelte';
 
 	/** @type {import('./$types').PageData} */
 	export let data: any;
@@ -36,13 +37,16 @@
 	<Section>
 		<div class="rounded-xl bg-white p-6 shadow sm:p-10">
 			{#if data.post.projectImageHref}
-				<div class="mb-16">
+				<Preload
+					class="mb-16 overflow-hidden rounded-lg"
+					src={data.post.projectImageHref}
+				>
 					<img
 						alt={data.projectImageAlt}
-						class="mx-auto"
+						class="mx-auto block"
 						src={data.post.projectImageHref}
 					/>
-				</div>
+				</Preload>
 			{/if}
 			<div class="markdown box-content">
 				<h2 class="!mt-0" id="introduction">Introduction</h2>
