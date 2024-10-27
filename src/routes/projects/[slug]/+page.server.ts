53f6d043a5e263b5d310c7c820cc69702802d48a
Jack Reimers <jack.reimers.00@gmail.com>
2024-10-27 15:21:55 +1100

    Fix typescript issue

--- src/routes/projects/[slug]/+page.server.ts
+++ src/routes/projects/[slug]/+page.server.ts
@@ -1,8 +1,9 @@
 import type { PageServerLoad } from './$types';
+import type { ProjectPost } from '$lib/interfaces/interfaces';
 import { getPost } from '$lib/functions/functions.posts';
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
 	return {
-		post: await getPost(fetch, params.slug, 'projects')
+		post: (await getPost(fetch, params.slug, 'projects')) as ProjectPost
 	};
 };
